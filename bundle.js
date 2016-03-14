fff545acf199fe4b5877779e98a367de8e927800
GoCD <ecprod@economist.com>
2016-03-14 13:22:08 +0000

    publish: Merge pull request #87 from economist-components/greenkeeper-postcss-cssnext-2.5.1
    generated from commit db7860780af1eb4441da7c8ee353543ed9525f0f

--- bundle.js
+++ bundle.js
@@ -1784,17 +1784,17 @@ module.exports = checkGlobal;
 },{"_process":454}],9:[function(require,module,exports){
 'use strict';
 
-var _inherits = require('babel-runtime/helpers/inherits')['default'];
-
-var _createClass = require('babel-runtime/helpers/create-class')['default'];
-
-var _classCallCheck = require('babel-runtime/helpers/class-call-check')['default'];
-
-var _extends = require('babel-runtime/helpers/extends')['default'];
-
-var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
-
 exports.__esModule = true;
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _react = require('react');
 
@@ -1907,7 +1907,7 @@ var Icon = (function (_React$Component) {
 exports['default'] = Icon;
 module.exports = exports['default'];
 
-},{"babel-runtime/helpers/class-call-check":370,"babel-runtime/helpers/create-class":372,"babel-runtime/helpers/extends":374,"babel-runtime/helpers/inherits":375,"babel-runtime/helpers/interop-require-default":376,"react":"react"}],10:[function(require,module,exports){
+},{"react":"react"}],10:[function(require,module,exports){
 'use strict';
 
 exports.__esModule = true;
@@ -57924,7 +57924,7 @@ var Navigation = (function (_React$Component) {
             { className: 'navigation__user-menu-linklist-item' },
             _react2['default'].createElement(
               'a',
-              { className: 'navigation__user-menu-linklist-link', href: '/subscriptions/renew' },
+              { className: 'navigation__user-menu-linklist-link', href: '/products/renew' },
               'Renew my subscription'
             )
           ),
